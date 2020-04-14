53c9e1b49fed9aed3b8f78ad2708589d6e80eaf2
pedro299 <63642977+pedro299@users.noreply.github.com>
2020-04-14 12:27:03 +0200

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,38 @@
+package testeadordados;
+
+import java.util.Scanner;
+
+public class TesteadorDados {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, b, c, d, e, f;
+        a = b = c = d = e = f = 0;
+        int n;
+        int aux = 0;
+        System.out.println("Veces del test");
+        n = scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            aux = scan.nextInt();
+            if (aux == 1) {
+                a++;
+            } else if (aux == 2) {
+                b++;
+            } else if (aux == 3) {
+                c++;
+            } else if (aux == 4) {
+                d++;
+            } else if (aux == 5) {
+                e++;
+            } else f++;
+        }
+        
+        System.out.println("Uno: "+a);
+        System.out.println("Dos: "+b);
+        System.out.println("Tres: "+c);
+        System.out.println("Cuatro: "+d);
+        System.out.println("Cinco: "+e);
+        System.out.println("Seis: "+f);
+    }
+
+}
